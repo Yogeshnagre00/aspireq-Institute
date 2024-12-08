b2197fb11c80c63f31eca66ef2ff1b07041a5443
Yogesh Nagre <yogeshnagre000@gmail.com>
2024-12-08 18:32:48 +0530

    update page

--- src/Pages/Courses/courses.jsx
+++ src/Pages/Courses/courses.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import Navbar from "../../components/Header/header";
-import "./courses.css";
-import CourseSection from "./coursesPage";
 import { Footer } from "../../components/Footer/footer";
+import "./courses.css";
+import CourseCard from "./CourseCard";
 
 const QASection = () => {
   // Slider items with images and videos
@@ -35,6 +35,80 @@ const QASection = () => {
       src: "./Images/Business analyst.jpg",
       title: "Office 1",
       description: "Description for business analyst",
+    },
+  ];
+  const courses = [
+    {
+      image: "./Images/Container.png",
+      category: "Development",
+      title: "Full Stack Developer",
+      lessons: 12,
+      duration: "1h 30m",
+      students: 20,
+      author: "Angela",
+      authorImage: "./Images/author.png",
+      price: 60,
+      rating: 4.5,
+    },
+    {
+      image: "./Images/Container.png",
+      category: "Development",
+      title: "Business Analyst(BA)",
+      lessons: 10,
+      duration: "2h 00m",
+      students: 15,
+      author: "John Doe",
+      authorImage: "./Images/author.png",
+      price: 45,
+      rating: 4,
+    },
+    {
+      image: "./Images/Container.png",
+      category: "Development",
+      title: "Software Development Engineer in Testing ",
+      lessons: 15,
+      duration: "2h 30m",
+      students: 30,
+      author: "Jane Smith",
+      authorImage: "./Images/author.png",
+      price: 70,
+      rating: 4.7,
+    },
+    {
+      image: "./Images/Container.png",
+      category: "Development",
+      title: "Devops Engineer",
+      lessons: 15,
+      duration: "2h 30m",
+      students: 30,
+      author: "Jane Smith",
+      authorImage: "./Images/author.png",
+      price: 70,
+      rating: 3.8,
+    },
+    {
+      image: "./Images/Container.png",
+      category: "Development",
+      title: "Corporate Training",
+      lessons: 15,
+      duration: "2h 30m",
+      students: 25,
+      author: "Jane Smith",
+      authorImage: "./Images/author.png",
+      price: 70,
+      rating: 4.5,
+    },
+    {
+      image: "/./Images/Container.png",
+      category: "Development",
+      title: "Scrum Master",
+      lessons: 15,
+      duration: "2h 30m",
+      students: 23,
+      author: "Jane Smith",
+      authorImage: "./Images/author.png",
+      price: 70,
+      rating: 4.5,
     },
   ];
 
@@ -160,9 +234,14 @@ const QASection = () => {
           </div>
         </div>
       </section>
-      <div>
-        <CourseSection />
+      <section className="course-section">
+      <h2>Courses</h2>
+      <div className="course-section__grid">
+        {courses.map((course, index) => (
+          <CourseCard key={index} {...course} />
+        ))}
       </div>
+    </section>
       <section>
         <div className="how-it-works">
           <h2>How it works</h2>
