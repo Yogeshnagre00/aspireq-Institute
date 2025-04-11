5bf9f5af18643548717e7549e6e0074bca3de571
Yogesh Nagre <yogeshnagre000@gmail.com>
2025-04-11 13:21:45 +0530

    add base in vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   build: {
     minify: "terser", // Use Terser for better minification
