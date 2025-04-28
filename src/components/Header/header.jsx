764bfb61158a561df179e6530a06a5a41c8d3868
Yogesh Nagre <yogeshnagre000@gmail.com>
2025-04-28 17:49:53 +0530

    update header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./header.css";
 
@@ -6,6 +6,7 @@ const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrollingUp, setIsScrollingUp] = useState(true);
 
   const handleLetsTalkClick = () => {
     if (location.pathname !== "/contactUs") {
@@ -17,11 +18,32 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Helper function to determine if a link is active
   const isActive = (path) => (location.pathname === path ? "active-link" : "");
 
+  useEffect(() => {
+    let lastScrollTop = window.pageYOffset;
+
+    const handleScroll = () => {
+      const currentScrollTop = window.pageYOffset;
+      if (currentScrollTop < lastScrollTop) {
+        // Scrolling Up
+        setIsScrollingUp(true);
+      } else {
+        // Scrolling Down
+        setIsScrollingUp(false);
+      }
+      lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={!isScrollingUp ? "hide" : ""}>
       <nav className="navbar">
         <div className="logo">
           <img src="/Images/aspireq green.webp" alt="Logo" />
@@ -49,6 +71,7 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
+
         <a
           href="#"
           className={`btn ${
@@ -58,6 +81,7 @@ const Navbar = () => {
         >
           Let&apos;s Connect
         </a>
+
         <div className="hamburger" onClick={toggleMenu}>
           &#9776;
         </div>
